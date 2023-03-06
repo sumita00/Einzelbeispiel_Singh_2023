c4bb1c24e026909eab0684c19a2b1ba5f9b3223a
sumita00 <101045757+sumita00@users.noreply.github.com>
2023-03-06 11:04:16 +0100

    Design erstellt

--- app/src/main/java/com/example/einzelbeispiel_singh_2023/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel_singh_2023/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.einzelbeispiel_singh_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView antwortFromServer;
+    private TextView berechnungresult;
+    private EditText martikelNr;
+    private Button sendButton;
+    private Button berechnen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
