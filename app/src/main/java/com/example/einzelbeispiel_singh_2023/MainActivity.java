15659b7bb9c25a91a696b71da8ce5e217e05c990
sumita00 <101045757+sumita00@users.noreply.github.com>
2023-03-12 11:07:38 +0100

    Handeler mit eingebaut für Verbindung

--- app/src/main/java/com/example/einzelbeispiel_singh_2023/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel_singh_2023/MainActivity.java
@@ -17,17 +17,17 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView antwortFromServer;
-    private TextView berechnungresult;
-    private EditText eingabemartikelNr;
-    private Button sendButton;
-    private Button berechnen;
-    boolean prim=true;
+     TextView antwortFromServer;
+     TextView berechnungresult;
+     EditText eingabemartikelNr;
+     Button sendButton;
+     Button berechnen;
+    boolean prim = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //UI-Elemente der Variablen zu weisen
 
         berechnungresult = (TextView) findViewById(R.id.textView2); // Testfeld fuer Berechnungsausgabe
 
@@ -38,14 +38,14 @@ public class MainActivity extends AppCompatActivity {
         sendButton = (Button) findViewById(R.id.button2); // Button fuer Server
         berechnen = (Button) findViewById(R.id.button); // Button fuer Berechnung
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        sendButton.setOnClickListener(new View.OnClickListener() { // Klick-Eventhandler setzen für TCP-Verbindung
             @Override
             public void onClick(View view) {
                 run();
             }
         });
 
-        berechnen.setOnClickListener(new View.OnClickListener() {
+        berechnen.setOnClickListener(new View.OnClickListener() { // Klick-Eventhandler setzen für Berechnungsbutton/Berechnungsausgabe
             @Override
             public void onClick (View view){
 
@@ -57,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < einzelneZiffer.length; i++) {
                     prim = true;
                     isPrime(Integer.parseInt(String.valueOf(einzelneZiffer[i]))); //Methode isPrime aufrufen ueber charArray darueber laufen lassen
-                    if( prim == true){ //wenn prime ture soll an String angehaengt werden von der jeweiligen Stell eim Array
-                        isPrimeZahlErgebnis += einzelneZiffer[i];
+                     if( prim == true) { //wenn prime ture soll an String angehaengt werden von der jeweiligen Stell eim Array
+                        isPrimeZahlErgebnis = isPrimeZahlErgebnis + einzelneZiffer[i];
                     }
                 }
                 berechnungresult.setText(isPrimeZahlErgebnis); //Ergebnis soll auf Console ausgegeben werden
@@ -79,26 +79,39 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void run() {
-        ExampleThread thread = new ExampleThread();
-        thread.start();
+        ExampleThread thread = new ExampleThread(eingabemartikelNr.getText().toString());
+        new Thread(thread).start();
     }
-    class ExampleThread extends Thread{
+    class ExampleThread implements Runnable {
+        private String matrikelnummer;
+        private Handler messagerecivehandler = new Handler();
+
+        ExampleThread(String matrikelnummer) {
+            this.matrikelnummer = matrikelnummer;
+        }
         @Override
         public void run() {
             try {
-                String inputUser = eingabemartikelNr.getText().toString(); // input vom Eingabefeld ablesen
+                String inputUser = matrikelnummer; // input vom Eingabefeld ablesen
 
                 Socket clientSocket = new Socket("se2-isys.aau.at", 53212); // client socket mit server verbinden
 
-                DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream()); // output stream zum Socket anhaengen
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); // output stream zum Socket anhaengen
 
-                BufferedReader inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //  input stream zum Socket anhaengen
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //  input stream zum Socket anhaengen
 
-                outputStream.writeBytes(inputUser + "\n"); // Input zum Server schicken
+                outToServer.writeBytes(inputUser + "\n"); // Input zum Server schicken
 
-                String result = inputStream.readLine(); // vom Server Input ablesen
+                final String modifiedSentence = inFromServer.readLine(); // vom Server Input ablesen
 
-                antwortFromServer.setText(result); //  result in textview schreiben
+
+                messagerecivehandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        antwortFromServer.setText("From Server:" + modifiedSentence);
+                    }
+                });
+
 
                 clientSocket.close(); //  client socket schließen
 
